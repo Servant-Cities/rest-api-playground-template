f92fa49b4d055db2f6c61453bf152dc915535666
Simon Mulquin <simon.mulquin@gmail.com>
2025-02-01 02:55:35 +0900

    feat: ✨ Adds preview UI

---
+++ src/lib/components/containers/preview-form.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	const defaultPreviewURL = `${page.url.protocol}//${page.url.host}/database`;
+	let urlInput = $state(defaultPreviewURL);
+	let previewURL = $state(defaultPreviewURL);
+</script>
+
+<form
+	class="flex-column h-[70vh] overflow-hidden border border-solid border-primary bg-primary text-primary-foreground"
+>
+	<div class="flex">
+		<Input bind:value={urlInput} class="rounded-none bg-primary text-primary-foreground" />
+		<Button
+			onclick={() => (previewURL = urlInput)}
+			variant="outline"
+			class="rounded-none bg-primary text-primary-foreground">Preview</Button
+		>
+	</div>
+	{#if previewURL}
+		<iframe title="preview" class="h-full w-full" src={previewURL}></iframe>
+	{/if}
+</form>
---
+++ src/routes/preview/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import PreviewForm from '$lib/components/containers/preview-form.svelte';
+</script>
+
+<PreviewForm />
